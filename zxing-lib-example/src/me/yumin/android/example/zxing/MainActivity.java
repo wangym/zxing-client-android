55c822aa1dcb4a90629ec375242998d841c77984
yumin <chinawym@gmail.com>
2013-05-12 23:28:26 +0800

    update

--- zxing-lib-example/src/me/yumin/android/example/zxing/MainActivity.java
+++ zxing-lib-example/src/me/yumin/android/example/zxing/MainActivity.java
@@ -1,22 +1,25 @@
+/**
+ * 
+ */
 package me.yumin.android.example.zxing;
 
+import com.google.zxing.client.android.CaptureActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
+/**
+ * @author yumin
+ * 
+ */
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		Intent intent = new Intent();
+		intent.setClass(this, CaptureActivity.class);
+		startActivity(intent);
+		finish();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
 }
