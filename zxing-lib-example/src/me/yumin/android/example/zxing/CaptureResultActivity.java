b9e55981648122db86d23ff00d8fca058df73328
yumin <chinawym@gmail.com>
2014-04-19 21:39:59 +0800

    update

--- zxing-lib-example/src/me/yumin/android/example/zxing/CaptureResultActivity.java
+++ zxing-lib-example/src/me/yumin/android/example/zxing/CaptureResultActivity.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package me.yumin.android.example.zxing;
 
@@ -11,40 +11,39 @@ import android.widget.TextView;
 
 /**
  * @author yumin
- * 
  */
 public class CaptureResultActivity extends Activity {
 
-	/**
-	 * 
-	 */
-	private TextView tvResult;
-	private TextView tvResultFormat;
-	private TextView tvUri;
+    /**
+     *
+     */
+    private TextView tvResult;
+    private TextView tvResultFormat;
+    private TextView tvUri;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_capture_result);
-		initActivity();
-		initData();
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_result);
+        initActivity();
+        initData();
+    }
 
-	private void initActivity() {
+    private void initActivity() {
 
-		tvResult = (TextView) findViewById(R.id.tv_result);
-		tvResultFormat = (TextView) findViewById(R.id.tv_result_format);
-		tvUri = (TextView) findViewById(R.id.tv_uri);
-	}
+        tvResult = (TextView) findViewById(R.id.tv_result);
+        tvResultFormat = (TextView) findViewById(R.id.tv_result_format);
+        tvUri = (TextView) findViewById(R.id.tv_uri);
+    }
 
-	private void initData() {
+    private void initData() {
 
-		Intent intent = getIntent();
-		if (null != intent) {
-			tvResult.setText(intent.getStringExtra(Intents.Scan.RESULT));
-			tvResultFormat.setText(intent.getStringExtra(Intents.Scan.RESULT_FORMAT));
-			tvUri.setText(intent.toUri(intent.getFlags()));
-		}
-	}
+        Intent intent = getIntent();
+        if (null != intent) {
+            tvResult.setText(intent.getStringExtra(Intents.Scan.RESULT));
+            tvResultFormat.setText(intent.getStringExtra(Intents.Scan.RESULT_FORMAT));
+            tvUri.setText(intent.toUri(intent.getFlags()));
+        }
+    }
 }
--- zxing-lib-example/src/me/yumin/android/example/zxing/MainActivity.java
+++ zxing-lib-example/src/me/yumin/android/example/zxing/MainActivity.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package me.yumin.android.example.zxing;
 
@@ -13,70 +13,69 @@ import android.content.Intent;
 
 /**
  * @author yumin
- * 
  */
 public class MainActivity extends Activity {
 
-	private static final int REQUEST_CODE = 200;
+    private static final int REQUEST_CODE = 200;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		initActivity();
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initActivity();
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-		if (null != data && requestCode == REQUEST_CODE) {
-			switch (resultCode) {
-			case Activity.RESULT_OK:
-				data.setClass(this, CaptureResultActivity.class);
-				startActivity(data);
-				break;
-			default:
-				break;
-			}
-		}
-	}
+        if (null != data && requestCode == REQUEST_CODE) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    data.setClass(this, CaptureResultActivity.class);
+                    startActivity(data);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
-	private void initActivity() {
+    private void initActivity() {
 
-		Button btnScan = (Button) findViewById(R.id.scan);
-		btnScan.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				doZXing(null);
-			}
-		});
-		Button btnScanGbk = (Button) findViewById(R.id.scan_gbk);
-		btnScanGbk.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				doZXing("GBK");
-			}
-		});
-		Button btnScanUtf8 = (Button) findViewById(R.id.scan_utf_8);
-		btnScanUtf8.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				doZXing("UTF-8");
-			}
-		});
-	}
+        Button btnScan = (Button) findViewById(R.id.scan);
+        btnScan.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doZXing(null);
+            }
+        });
+        Button btnScanGbk = (Button) findViewById(R.id.scan_gbk);
+        btnScanGbk.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doZXing("GBK");
+            }
+        });
+        Button btnScanUtf8 = (Button) findViewById(R.id.scan_utf_8);
+        btnScanUtf8.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doZXing("UTF-8");
+            }
+        });
+    }
 
-	private void doZXing(String characterSet) {
+    private void doZXing(String characterSet) {
 
-		Intent intent = new Intent();
-		intent.setAction(Intents.Scan.ACTION);
-		// intent.putExtra(Intents.Scan.MODE, Intents.Scan.QR_CODE_MODE);
-		intent.putExtra(Intents.Scan.CHARACTER_SET, characterSet);
-		intent.putExtra(Intents.Scan.WIDTH, 600);
-		intent.putExtra(Intents.Scan.HEIGHT, 600);
-		intent.putExtra(Intents.Scan.PROMPT_MESSAGE, "type your prompt message");
-		intent.setClass(this, CaptureActivity.class);
-		startActivityForResult(intent, REQUEST_CODE);
-	}
+        Intent intent = new Intent();
+        intent.setAction(Intents.Scan.ACTION);
+        // intent.putExtra(Intents.Scan.MODE, Intents.Scan.QR_CODE_MODE);
+        intent.putExtra(Intents.Scan.CHARACTER_SET, characterSet);
+        intent.putExtra(Intents.Scan.WIDTH, 600);
+        intent.putExtra(Intents.Scan.HEIGHT, 600);
+        intent.putExtra(Intents.Scan.PROMPT_MESSAGE, "type your prompt message");
+        intent.setClass(this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
 }
